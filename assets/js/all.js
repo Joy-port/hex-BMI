5915895ed418bd9d34d6fd36fe975412e505a767
Joy Cheng <joy.cheng97324@gmail.com>
2021-10-04 23:23:21 +0800

    Update 2021-10-04T15:23:19.990Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -13,7 +13,7 @@ var resultNum = document.querySelector(".result-num");
 var resultMsg = document.querySelector(".result-msg");
 var list = document.querySelector(".list");
 var clearAllBtn = document.querySelector(".clear-all-btn");
-var data = JSON.parse(localStorage.getItem('BMI calculate')) || [];
+var data = JSON.parse(localStorage.getItem('BMI Record')) || [];
 var level = ''; //== 產生列表 ==//
 
 function updateData(data) {
@@ -22,7 +22,7 @@ function updateData(data) {
   if (data.length > 0) {
     clearAllBtn.style.display = 'block';
     data.forEach(function (item) {
-      var content = "\n        <li class=\"".concat(item.level, "\">\n          <h3>").concat(item.msg, "</h3>\n          <div>\n            <small>BMI</small>\n            <p data-type=\"bmiNum\">").concat(item.bmi, "</p>\n          </div>\n          <div>\n            <small>weight</small>\n            <p data-type=\"weightNum\">").concat(item.weight, "</p>\n          </div>\n          <div>\n            <small>height</small>\n            <p data-type=\"heightNum\">").concat(item.height, "</p>\n          </div>\n          <small>").concat(item.date, "</small>\n          <a href=\"#\" class=\"clear-btn\">\n            <i class=\"material-icons-outlined\"> highlight_off </i>\n          </a>\n        </li>");
+      var content = "\n        <li class=\"".concat(item.level, "\" data-id=\"").concat(item.time, "\">\n          <h3>").concat(item.msg, "</h3>\n          <div>\n            <small>BMI</small>\n            <p>").concat(item.bmi, "</p>\n          </div>\n          <div>\n            <small>weight</small>\n            <p>").concat(item.weight, "</p>\n          </div>\n          <div>\n            <small>height</small>\n            <p>").concat(item.height, "</p>\n          </div>\n          <small>").concat(item.date, "</small>\n          <a href=\"#\" class=\"delete-btn\">\n            <i class=\"material-icons-outlined\"> highlight_off </i>\n          </a>\n        </li>");
       str += content;
     });
   } else {
@@ -36,6 +36,8 @@ function updateData(data) {
 inputHeight.addEventListener('keyup', inputVerify, false);
 inputWeight.addEventListener('keyup', inputVerify, false);
 resultBtn.addEventListener('click', newStatus, false);
+resetBtn.addEventListener('click', resetAll, false);
+list.addEventListener('click', deleteBtn, false);
 
 function newStatus() {
   if (inputVerify() === "true") {
@@ -185,10 +187,43 @@ function inputVerify() {
   verifyWeight.classList.remove("visible");
   inputHeight.classList.remove("warning");
   inputWeight.classList.remove("warning");
+} //== 清空表單、按鈕 ==//
+
+
+function resetAll() {
+  inputHeight.value = "";
+  inputWeight.value = "";
+  resultBtn.style.display = 'block';
+  showResult.style.display = 'none';
+  showResult.setAttribute("class", "");
 } //== 更新localStorage ==//
 
 
 function updateLocalStorage(data) {
-  localStorage.setItem('BMI', JSON.stringify(data));
+  localStorage.setItem('BMI Record', JSON.stringify(data));
+} // 單一刪除按紐 //
+
+
+function deleteBtn(e) {
+  if (!e.target.classList.contains("material-icons-outlined")) {
+    return;
+  }
+
+  ;
+  e.preventDefault();
+  var id = parseInt(e.target.closest("LI").dataset.id);
+  var deleteData = data.findIndex(function (item) {
+    return item.time === id;
+  });
+  data.splice(deleteData, 1);
+  updateLocalStorage(data);
+  updateData(data);
+} // 全部刪除按鈕 //
+
+
+function init() {
+  updateData(data);
 }
+
+init();
 //# sourceMappingURL=all.js.map
